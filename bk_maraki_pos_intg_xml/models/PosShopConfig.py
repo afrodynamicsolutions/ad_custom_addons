f76e5f495b3168b45e2a5c15b073d9c9fc108316
afrodynamicsolutions <afrodynamicsolutions@gmail.com>
2024-10-06 16:28:53 +0300

    Delete bk_maraki_pos_intg_xml directory

--- bk_maraki_pos_intg_xml/models/PosShopConfig.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class PosShopConfig(models.Model):
-    _inherit= "pos.config"
-
-    pos_xml_dump= fields.One2many("bk.pos.xml_dump","id",help="related xml damp locations")
--- bk_maraki_pos_intg_xml/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pos_order
-from . import bk_pos_xml_dump
-from . import PosShopConfig
